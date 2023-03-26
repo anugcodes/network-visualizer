5b8535db352e2a4fe325cbd2808efd42dc1a9696
Anurag Biswal <anugbswl@gmail.com>
2023-03-26 14:13:52 +0530

    initial project setup

---
+++ ihr_testing/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>  
+</template>
+
+<style scoped>
+
+</style>
