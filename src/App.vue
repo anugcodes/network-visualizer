44c392ba34a311134182bd518ee69f30c5532a22
Anurag Biswal <anugbswl@gmail.com>
2023-03-27 18:56:20 +0530

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Network Dependency visualizer using Sigma js</h2>
+    <div class="container">
+      <InputForm />
+      <AsHegeGraph as-number="string" timestamp="timestamp" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AsHegeGraph from './components/AsHegeGraph.vue';
+import InputForm from './components/InputForm.vue';
+export default {
+  name: "App",
+  data() {
+    return {}
+  },
+  methods: {},
+  components: {
+    AsHegeGraph,
+    InputForm,
+  }
+};
+</script>
+
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: .5rem;
+}
+@media screen and (max-width: 1000px) {
+  .container{
+    display: block;
+  }
+}
+
+h2 {
+  text-align: center;
+  color: whitesmoke;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/AsHegeGraph.vue
@@ -0,0 +1,41 @@
+
+<template>
+    <div class="graph-container">
+        graph goes here.
+        <div id="graph-holder">
+            actual graph is here.
+        </div>
+    </div>
+</template>
+
+<script>
+import Sigma from "sigma";
+import Graph from "graphology";
+export default {
+    name: 'AsHegeGraph',
+    props: {
+        asNumber: { required: true, type: String },
+        timestamp: {required: true, type: String },
+        timestamp__lte: {required: false, type: String },
+        timestamp__gte: {required: false, type: String },
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {},
+}
+
+</script>
+
+<style scoped>
+.graph-container {
+    width: 100%;
+    margin: .5rem;
+    padding: .5rem;
+    background-color: honeydew;
+    opacity: .8;
+    color: black;
+}
+</style>
