a34b99968aa808370b02703bf69f6524be982279
Anurag Biswal <anugbswl@gmail.com>
2023-03-28 11:08:23 +0530

    installed layout form graphology

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h2>Network Dependency visualizer using Sigma js</h2>
     <div class="container">
+      <h2>Network Dependency visualizer using Sigma js</h2>
       <!-- form for values to plot in graph: -->
       <form @submit.prevent="handleSubmit">
         <div class="form-control">
@@ -10,39 +10,104 @@
         </div>
         <div class="form-control">
           <label for="asn">select date: </label>
-          <input type="date" v-model="date"/>
+          <input type="date" v-model="date" />
         </div>
-        <button type="submit" >plot</button>
+        <button type="submit">plot</button>
       </form>
+
       <!-- graph component -->
-      <AsHegeGraph :as-number="asNumber" :timestamp="date" />
+      <div class="graph-container">
+        <p>plotting the graph for asn:
+          <span>{{ asNumber }}</span>
+          and for date:
+          <span>{{ date }}</span>
+        </p>
+        <!-- this div will be used to plot graph -->
+        <div id="graph-holder" ref="graphHolder"></div>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
-import AsHegeGraph from './components/AsHegeGraph.vue';
+import axios from 'axios';
+import Sigma from "sigma";
+import Graph from "graphology";
+import { circular } from 'graphology-layout';
 
+const HEGE_BASE_URL = "https://ihr.iijlab.net/ihr/api/hegemony";
 
 export default {
   name: "App",
   components: {
-    AsHegeGraph,
   },
   data() {
     return {
       date: new Date().toISOString().split('T')[0],
       asNumber: "",
+      hegemony_data: [],
     }
   },
   methods: {
-    handleSubmit(){
-      console.log("values from form: ",this.asNumber," --- ",this.date)
+    async handleSubmit() {
+      console.log("values from form: ", this.asNumber, " --- ", this.date);
+      console.log("calling api using axios...");
+      await this.loadDate();
+      this.plotGraph();
     },
-    
+
+    // call hegemony api to load hegemony data.
+    async loadDate() {
+      if (this.asNumber === '0') return;
+      let timestamp = new Date(this.date).toISOString();
+      console.log(timestamp);
+      const res = await axios.get(HEGE_BASE_URL, {
+        params: {
+          originasn: this.asNumber,
+          timebin: timestamp,
+        }
+      })
+      this.hegemony_data = res.data.results;
+    },
+
+    plotGraph() {
+      const graph = new Graph({
+        multi: true,
+        allowSelfLoops: true,
+        type: "directed"
+      });
+      const container = this.$refs.graphHolder;
+      const rendrer = new Sigma(graph, container, {
+        settings: {
+          minArrowSize: 10,
+        }
+      });
+
+      this.hegemony_data.forEach(ele => {
+        if (ele.originasn !== ele.asn) {
+          graph.addNode(ele.asn, {
+            size: Math.floor(ele.hege * 100), label: ele.asn, color: 'red',
+          })
+        }
+        else {
+          graph.addNode(ele.asn, {
+            size: Math.floor(ele.hege * 50), label: ele.asn, color: 'green',
+          })
+        }
+      }),
+        this.hegemony_data.forEach(ele => {
+          graph.addEdge(ele.originasn, ele.asn, {
+            type: 'arrow',
+            size: 5,
+          });
+        })
+      circular.assign(graph);
+      rendrer.refresh();
+      console.log(rendrer);
+    },
   },
 };
-
 </script>
 
 
@@ -50,31 +115,52 @@ export default {
 .container {
   padding: .5rem;
 }
+
 @media screen and (max-width: 800px) {
   .container {
     display: block;
   }
 }
+
 h2 {
   font-size: 1.25rem;
-  color: whitesmoke;
   padding: .5rem;
+  border-bottom: 1px solid black;
 }
-form{
+
+form {
   display: flex;
   align-items: flex-end;
-  border-bottom: 2px dotted white;
   padding: .5rem 0;
 }
-.form-control{
+
+.form-control {
   margin: 0 .5rem;
   display: flex;
   flex-direction: column;
 }
-input{
+
+input {
   width: 8rem;
 }
-button{
+
+button {
   padding: 0 .5rem;
+}
+
+.graph-container {
+  background-color: beige;
+  color: black;
+  margin-top: .5rem;
+  padding: .25rem;
+}
+
+span {
+  font-style: italic;
+  font-weight: bold;
+}
+
+#graph-holder {
+  height: 32rem;
 }
 </style>
